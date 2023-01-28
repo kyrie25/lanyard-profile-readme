c9a0acb9f11fb70751039726723e6ec70a7bb9a7
Nam Anh <phamnamanh25@gmail.com>
2023-01-28 16:41:51 +0700

    fix: string termination

--- src/renderCard.tsx
+++ src/renderCard.tsx
@@ -308,7 +308,7 @@ const renderCard = async (body: LanyardTypes.Root, params: Parameters): Promise<
                                 height: 21px;
                                 color: #7289da;
                                 content: "";
-                                filter: invert(54%) sepia(90%) saturate(574%) hue-rotate(198deg) brightness(88%) contrast(93%);
+                                filter: invert(54%) sepia(90%) saturate(574%) hue-rotate(198deg) brightness(88%) contrast(93%);"
                             </div>
                             <div style="
                                 display: flex;
